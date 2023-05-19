8b97bdc4841113c351eb9c6a48f423c654b2ef3c
Marwa EL KAMIL <87017143+maghwa@users.noreply.github.com>
2023-05-19 12:02:23 +0100

    Add files via upload
     this code performs a specific transformation on a matrix M by rearranging its elements based on calculations involving the row and column indices. The resulting transformed matrix B is generated by copying the elements of M according to the computed positions.

---
+++ transformation.m
@@ -0,0 +1,29 @@
+function [B] =transformation(M)
+[rows,cloms]=size(M);
+
+for i=1:rows
+    for j=1:cloms
+        % Cela permet de calculer la position de la ligne correspondante dans la matrice B 
+        %après transformation.
+     if(mod(i,2)==0)
+            k1(i)=(i+cloms)/2;
+     else (mod(i,2)~=0)
+             k1(i)=(i+1)/2;
+     end
+     if(mod(j,2)==0)
+            k2(j)=(j+cloms)/2;
+      else
+            k2(j)=(j+1)/2;
+     end
+    end
+end 
+%Ces boucles for sont utilisées pour copier les éléments de la matrice M dans la matrice B 
+%après transformation. Les positions des éléments dans B sont déterminées
+%à l'aide des tableaux k1 et k2 calculés précédemment.
+for i=1:rows
+    for j=1:cloms
+     B(k1(i),k2(j))=M(i,j);
+    end
+end 
+
+end
